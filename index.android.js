08aa611c8b05a6da400a39d48a64fcdafe1bb28f
Robert Lancer <rob@collaborizm.com>
2017-01-18 18:41:36 -0500

    Using a tab view component for navigation

--- index.android.js
+++ index.android.js
@@ -3,40 +3,14 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, {Component} from 'react';
+import {AppRegistry, View, Text} from 'react-native';
 import Entry from './lib/Entry';
 
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-
-export default class rn4test extends Component {
-    render() {
-      return <Entry/>;
-    }
+export default class CollaborizmCommunityApp extends Component {
+  render() {
+    return <Entry/>;
+  }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
-
-AppRegistry.registerComponent('rn4test', () => rn4test);
+AppRegistry.registerComponent('rn4test', () => CollaborizmCommunityApp);
--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,13 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+import React, {Component} from 'react';
+import Entry from './lib/Entry';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class rn4test extends Component {
+export default class CollaborizmCommunityApp extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return <Entry/>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('rn4test', () => rn4test);
+AppRegistry.registerComponent('CollaborizmCommunityApp', () => CollaborizmCommunityApp);
